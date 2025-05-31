6b7919efb237f9ae8425780b4df9d23e091b87cd
Don Matteo <think@unblog.ch>
2025-05-31 05:29:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# pmaproving
+
+This bash script provisioning phpMyAdmin on GNU/Linux, written in GNU bash v5.1.16(1) on Debian 11 and Ubuntu 22.04.3 LTS.
+
+## Preface
+
+I wrote pmaproving.sh primarily because deploying phpmyadmin from the apt repository proved impractical when using different PHP versions, especially in development environments. I use Apache2 in WSL. To avoid time-consuming setup with every installation and to always provide the same requirements quickly and smouth, I created this Bash script.
+
+## Purpose
+
+Provisioning of phpMyAdmin on your web server always with requirements quickly and smouth.
+
+## Usage:
+
+Run in bash as root.
+
+```
+cd ~
+wget -L https://raw.github.com/unblog/pmaproving/main/pmaproving.sh -O pmaproving.sh
+chmod 755 pmaproving.sh
+./pmaproving.sh
+```
+
+## Requirement
+
+No special requirements are expected; Apart from that, a ready-to-use web server with Apache and PHP, the unzip and wget packages will be provided automatically if not already exist.
+
+## Addendum
+
+Please leave a comment for suggestions, additions, deviations or troubleshooting.
---
+++ pmaproving.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+#
+#    $file:pmaproving.sh created 2025-05-30 12:19 PM CEST
+#    $Id:don
+#
+#    Don Matteo <think@unblog.ch> May 30, 2025.
+#    Copyright(c) 2010-2025 A-Enterprise GmbH.
+#    https://think.unblog.ch
+#
+#    Released under the GNU General Public License WITHOUT ANY WARRANTY.
+#    See LICENSE.TXT for details.
+#
+#    vim: expandtab sw=4 ts=4 sts=4:
+#
+###### SETTINGS ######
+MYUSER="pma"
+MYPASS="secret123"
+DATABASE="phpmyadmin"
+### END SETTINGS #####
+
+## PREPARE REQUIREMENTS
+if [ ! -f /usr/bin/unzip ]; then
+    apt install -y unzip
+fi
+if [ ! -f /usr/bin/wget ]; then
+    apt install -y wget
+fi
+
+## GET PHPMYADMIN PACKAGE AND UNPACKING
+cd /usr/share
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip -O phpmyadmin.zip
+unzip phpmyadmin.zip
+rm -f phpmyadmin.zip
+mv phpmyadmin-*-all-languages phpmyadmin
+chmod -R 0755 phpmyadmin
+mkdir /usr/share/phpmyadmin/tmp/
+chown -R www-data:www-data /usr/share/phpmyadmin/tmp/
+
+## CREATE APACHE CONFIG
+cat << EOF > /etc/apache2/conf-available/phpmyadmin.conf
+Alias /phpmyadmin /usr/share/phpmyadmin
+
+<Directory /usr/share/phpmyadmin>
+    Options SymLinksIfOwnerMatch
+    DirectoryIndex index.php
+</Directory>
+
+<Directory /usr/share/phpmyadmin/templates>
+    Require all denied
+</Directory>
+<Directory /usr/share/phpmyadmin/libraries>
+    Require all denied
+</Directory>
+<Directory /usr/share/phpmyadmin/setup/lib>
+    Require all denied
+</Directory>
+EOF
+
+a2enconf phpmyadmin
+systemctl reload apache2
+
+## CREATE PHPMYADMIN DATABASE AND USER CREDENTIALS
+# If /root/.my.cnf exists then it won't ask for root password
+if [ -f /root/.my.cnf ]; then
+
+    mysql -e "CREATE DATABASE ${DATABASE} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+    mysql -e "CREATE USER ${MYUSER}@localhost IDENTIFIED BY '${MYPASS}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${MYUSER}'@'localhost';"
+    mysql -e "FLUSH PRIVILEGES;"
+
+# If /root/.my.cnf doesn't exist then it'll ask for root password   
+else
+    echo "Please enter root user MySQL password!"
+    echo "Note: password will be hidden when typing"
+    read -sp rootpasswd
+    mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${DATABASE} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+    mysql -uroot -p${rootpasswd} -e "CREATE USER ${MYUSER}@localhost IDENTIFIED BY '${MYPASS}';"
+    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${MYUSER}'@'localhost';"
+    mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+fi
+
+mysql -uroot phpmyadmin < /usr/share/phpmyadmin/sql/create_tables.sql
+
+# create phpMyAdmin configuration from the saample file in the same way generating the blowfish_secret for cookie auth.
+# instead of - cp /usr/share/phpmyadmin/config.sample.inc.php /usr/share/phpmyadmin/config.inc.php
+# php -r 'echo bin2hex(random_bytes(32)) . PHP_EOL;'
+
+## ADD PHPMYADMIN SETTINGS
+# /usr/share/phpmyadmin/config.inc.php
+
+randomBlowfishSecret=$(php -r 'echo bin2hex(random_bytes(32)) . PHP_EOL;')
+sed -e "s|cfg\['blowfish_secret'\] = ''|cfg['blowfish_secret'] = sodium_hex2bin\('$randomBlowfishSecret'\)|" /usr/share/phpmyadmin/config.sample.inc.php > /usr/share/phpmyadmin/config.inc.php
+
+sed -i "/\/\/ \$cfg\['Servers'\]\[\$i\]\['controlpass'\] *= *'pmpass'/ {
+    s|^// ||;
+    s|'pmpass'|'${MYPASS//&/\\&}'|;
+}" config.inc.php
+
+sed -i "/\$cfg\['MaxRows'\] = 50;/ s#^//##" config.inc.php
+
+echo "Finish!"
+echo "http://localhost/phpmyadmin/"
